aaa7300f1a9b8abf42e0c4eda94efb48b1315b64
root <root@Leahs-LGgram.localdomain>
2025-07-20 15:42:31 -0400

    build folder is available

---
+++ build/CMakeFiles/sudoku.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lkerry/cpp_api/sudoku/arith.cpp" "CMakeFiles/sudoku.dir/arith.cpp.o" "gcc" "CMakeFiles/sudoku.dir/arith.cpp.o.d"
+  "/home/lkerry/cpp_api/sudoku/board.cpp" "CMakeFiles/sudoku.dir/board.cpp.o" "gcc" "CMakeFiles/sudoku.dir/board.cpp.o.d"
+  "/home/lkerry/cpp_api/sudoku/wrapper.cpp" "CMakeFiles/sudoku.dir/wrapper.cpp.o" "gcc" "CMakeFiles/sudoku.dir/wrapper.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
